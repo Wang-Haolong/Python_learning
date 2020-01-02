8e38c24057c1cc3d2609dd2eb1f7ed9152600a1a
WangHaolong <wanghaolong0@126.com>
2020-01-02 13:56:30 +0800

    optimalize the code of unhappy_J.py

--- unhappy_J.py
+++ unhappy_J.py
@@ -1,13 +1,7 @@
-i=0;
-week=[8]
 maxn=[0,8]
-while i<7:
-    i+=1
-    a,b=input().split()
-    a=int(a)
-    b=int(b)
-    week.append(a+b)
-    if week[i]>maxn[1]:
-        maxn[1]=week[i]
-        maxn[0]=i
+for i in range(7):
+    a,b=map(int,input().split())
+    if a+b>maxn[1]:
+        maxn[0]=i+1
+        maxn[1]=a+b
 print(maxn[0])
