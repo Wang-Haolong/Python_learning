ecb2cd22b88b1dda5fb19e657f8e3d2ee227abe2
WangHaolong <wanghaolong0@126.com>
2020-01-03 10:56:01 +0800

    optimalize the code of MT

--- MT.py
+++ MT.py
@@ -1,23 +1,8 @@
-class memory:
-    def __init__(self,size):
-        self.__SIZE=size
-        self.used=0
-        self.dic=[]
-    def addword(self,word):
-        if self.used^self.__SIZE:
-            self.dic.append(word)
-            self.used+=1
-        else:
-            self.dic.pop(0)
-            self.dic.append(word)
-    def check(self,word):
-        if word in self.dic:
-            return True
 m,n=map(int,input().split())
-mem=memory(m)
-ans=0
-for i in map(int,input().split()):
-    if not mem.check(i):
+mem=[]
+ans=used=0
+for i in filter(lambda x:x not in mem,map(int,input().split())):
+        if not len(mem)^m:mem.pop(0)
+        mem.append(i)
         ans+=1
-        mem.addword(i)
 print(ans)
