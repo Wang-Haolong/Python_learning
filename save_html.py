a03f0dceca6e5a6437406c9378bf59d78067c5d2
WangHaolong <wanghaolong0@126.com>
2020-01-03 22:00:47 +0800

    add a line in SH

--- save_html.py
+++ save_html.py
@@ -1,6 +1,7 @@
 import requests
-#goal=requests.get(input())
-goal=requests.get(input(),headers={'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit'})
+goal=requests.get(input())
+#伪iphone6:
+#goal=requests.get(input(),headers={'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit'})
 f=open(input(),'wb')
 f.write(goal.text.encode(goal.encoding))
 f.close()
