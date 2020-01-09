25a7140113786c8791e1e27054f2b81c6bfc3c6d
WangHaolong <wanghaolong0@126.com>
2020-01-09 13:45:26 +0800

    nothing

--- save_html.py
+++ save_html.py
@@ -1,5 +1,6 @@
 import requests
-goal=requests.get(input())
+headers=headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"}
+goal=requests.get(input(),headers=headers)
 #伪iphone6:
 #goal=requests.get(input(),headers={'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit'})
 f=open(input(),'wb')
