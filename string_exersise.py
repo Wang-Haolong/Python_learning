128e37aa6730d8debe25f62a43b87097fb16756b
WangHaolong-Home <wanghaolong0@126.com>
2020-01-11 12:37:28 +0800

    an exersise

--- string_exersise.py
+++ string_exersise.py
@@ -1,4 +1,7 @@
-message='hello worlD'
+message='hello worlD   \t'
 print(message.lower())
 print(message.upper())
 print(message.title())
+print(message+"hello world")
+message=message.rstrip()#remove trailing spaces and tabs
+print(message+"hello world")
