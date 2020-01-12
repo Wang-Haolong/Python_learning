acebb55ec863f9c1b7a5b1d8495c3e9faade6bae
WangHaolong <wanghaolong0@126.com>
2020-01-12 13:52:41 +0800

    did an exersise.

---
+++ input_exersise.py
@@ -0,0 +1,10 @@
+﻿print('Let me see if I can find you a '+input('What car do you want? ')+'.')
+if int(input('How many persons will have meal? '))>8:
+    print('We have no free table now.')
+else:
+    print('We have a free table right now.')
+num=int(input('Type in a number: '))
+if num%10:
+    print(str(num)+" is'nt a multiple of 10")
+else:
+    print(str(num)+" is a multiple of 10")
