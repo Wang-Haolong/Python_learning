804a195a2b1973def8fa9598feb6995760a750bc
WangHaolong-Home <wanghaolong0@126.com>
2020-02-01 22:24:06 +0800

    exersise

--- file_reader.py
+++ file_reader.py
@@ -1,3 +1,40 @@
-with open('pi_digits.txt') as file_object:
-    contents = file_object.read()
-    print(contents.rstrip())
+file_name='pi_digits.txt'
+
+with open(file_name) as file_object:
+    #contents = file_object.read()
+    #print(contents.rstrip())
+    for line in file_object:
+        print(line.rstrip())
+
+with open(file_name) as file_object:
+    lines = file_object.readlines()
+
+for line in lines:
+    print(line.rstrip())
+
+pi_string = ''
+
+for line in lines:
+    pi_string += line.strip()
+
+print(pi_string)
+print(len(pi_string))
+
+file_name='pi_million_digits.txt'
+
+with open(file_name) as file_object:
+    lines = file_object.readlines()
+
+pi_string = ''
+
+for line in lines:
+    pi_string += line.strip()
+
+print(pi_string[:52])
+print(len(pi_string))
+
+birthday = input('Enter your birthday, in the form mmddyy: ')
+if birthday in pi_string:
+    print('Your birthday appears in the first million digits of pi!')
+else:
+    print('Your birthday does not appear in the first million digits of pi.')
